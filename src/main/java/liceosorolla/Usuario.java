5f2ee24068ce65e358dd3f478bbd795bed7cd0a8
DAM_1 <DAM_1@PB04>
2020-03-06 21:04:09 +0100

    usuario bloque

--- src/main/java/liceosorolla/Usuario.java
+++ src/main/java/liceosorolla/Usuario.java
@@ -1,5 +1,74 @@
 package main.java.liceosorolla;
 
-public class Usuario {
+import java.time.LocalDate;
+import java.time.Year;
 
+public class Usuario {
+	private String nombre;
+	private String apellidos;
+	private LocalDate fechaNacimiento;
+	private String identificador;
+	
+	public Usuario(String nombre, String apellidos, LocalDate fechaNacimiento, String identificador) {
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.fechaNacimiento = fechaNacimiento;
+		this.identificador = identificador;
+	}
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		nombre = nombre;
+	}
+	public String getApellidos() {
+		return apellidos;
+	}
+	public void setApellidos(String apellidos) {
+		apellidos = apellidos;
+	}
+	public LocalDate getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+	public void setFechaNacimiento(LocalDate fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+	public String getIdentificador() {
+		return identificador;
+	}
+	public void setIdentificador(String identificador) {
+		identificador = identificador;
+	}
+	
+	public String toString() {
+		return "nombre " + nombre + ", apellidos " + apellidos + ", fechaNacimiento " + fechaNacimiento
+				+ ", identificador " + identificador;
+	}
+	
+	public String comprobarNombre(){
+		return this.nombre + " " + this.apellidos;
+	}
+	
+	public boolean mayorEdad() {
+		
+		boolean edad = true;
+		
+		if(Year.now().getValue() -18 >= fechaNacimiento.getYear()) {
+			edad=true;
+		}else {
+			edad=false;
+		}
+		return edad;
+	}
+	
+	public String nombreMayusculas() {
+		return this.nombre.toUpperCase();
+	}
+	
+	public String nombreMinisculas() {
+		return this.nombre.toLowerCase();
+	}
+	
 }
--- src/test/java/liceosorolla/UsuarioTest.java
+++ src/test/java/liceosorolla/UsuarioTest.java
@@ -1,5 +1,59 @@
 package test.java.liceosorolla;
 
-public class UsuarioTest {
+import static org.junit.Assert.assertEquals;
 
+import java.time.LocalDate;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import main.java.liceosorolla.Usuario;
+
+public class UsuarioTest {
+	
+	private Usuario usuario;
+	
+	@Before
+	public void before() {
+		usuario = new Usuario("Pirtmon", "Nova Nara", LocalDate.of(1999,16,10), "54296978G");
+	}
+	
+	@Test
+	public void compruebaNombre() {
+		assertEquals("Pirtmon", usuario.getNombre());
+	}
+	
+	@Test
+	public void compruebaApellidos() {
+		assertEquals("Nova Nara", usuario.getApellidos());
+	}
+	
+	@Test
+	public void compruebaNombreCompleta() {
+		
+		assertEquals("Alejandro Nova Nara", usuario.comprobarNombre());
+	}
+	
+	@Test
+	public void compruebaMayoriaDeEdad() {
+		assertEquals(true, usuario.mayorEdad());
+	}
+	
+	@Test
+	public void compruebaIdentificador() {
+		
+		assertEquals("54296978G", usuario.getIdentificador());
+	}
+	
+	@Test
+	public void compruebaNombreMayusculas() {
+		
+		assertEquals("PIRTMON", usuario.nombreMayusculas());
+	}
+	
+	@Test
+	public void compruebaNombreMinusculas() {
+		
+		assertEquals("pirtmon", usuario.nombreMinisculas());
+	}
 }
