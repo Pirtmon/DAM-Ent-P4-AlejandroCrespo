027ca298392f2c03110ef295b16deba23058a096
DAM_1 <DAM_1@PB06>
2020-03-06 20:45:50 +0100

    # WARNING: head commit changed in the meantime
    Calculado rectangulo

--- src/main/java/liceosorolla/Rectangulo.java
+++ src/main/java/liceosorolla/Rectangulo.java
@@ -1,5 +1,83 @@
 package main.java.liceosorolla;
 
 public class Rectangulo {
+	
+	private int lado1;
+	private int lado2;
+	
+	public Rectangulo(int lado1, int lado2) {
+		this.lado1 = lado1;
+		this.lado2 = lado2;
+	}
 
+	public int getLado1() {
+		return lado1;
+	}
+	
+	public void setLado1(int lado1) {
+		this.lado1 = lado1;
+	}
+	
+	public int getLado2() {
+		return lado2;
+	}
+	
+	public void setLado2(int lado2) {
+		this.lado2 = lado2;
+	}
+
+	public boolean calcularLadosIguales() {
+		
+		boolean lados=true;
+		
+		if (lado1==lado2) {
+			lados=true;
+		}else {
+			lados=false;
+		}
+		
+		return lados;
+		
+	}
+	
+	public int calcularPerimetro() {
+		
+		int resultado;
+		
+		resultado = (this.lado1+this.lado2)*2;
+		
+		return resultado;
+		
+	}
+	
+	public int calcularArea() {
+		
+		int resultado;
+		
+		resultado = this.lado1*this.lado2;
+		
+		return resultado;
+		
+	}
+	
+	public int calcularDiagonal() {
+		
+		int resultado;
+		
+		resultado= (int) Math.sqrt((Math.pow(this.lado1, 2))+(Math.pow(this.lado2, 2)));
+		
+		return resultado;
+	} 
+	
+	public int calcularCircunferencia() {
+		
+		int radio;
+		int menor = this.lado1;
+		
+		if(this.lado2 < this.lado1) {
+			menor=this.lado2;
+		}
+		return radio = menor/2;
+			
+	}
 }
--- src/test/java/liceosorolla/RectanguloTest.java
+++ src/test/java/liceosorolla/RectanguloTest.java
@@ -1,5 +1,43 @@
 package test.java.liceosorolla;
 
-public class RectanguloTest {
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import main.java.liceosorolla.Rectangulo;
+
+public class RectanguloTest {
+	
+	private Rectangulo rectangulo;
+	
+	@Before
+	public void before() {
+		rectangulo = new Rectangulo(5,6);
+	}
+
+	@Test
+	public void compruebaLadosIguales() {
+		assertEquals(false, rectangulo.calcularLadosIguales());
+	}
+	
+	@Test
+	public void compruebaPerimetro() {
+		assertEquals(22, rectangulo.calcularPerimetro());
+	}
+	
+	@Test
+	public void compruebaArea() {
+		assertEquals(30, rectangulo.calcularArea());
+	}
+	
+	@Test
+	public void comprobarDiagonal() {
+		assertEquals(7, rectangulo.calcularDiagonal());
+	}
+	
+	@Test
+	public void comprobarCircunferencia() {
+		assertEquals(2, rectangulo.calcularCircunferencia());
+	}
 }
