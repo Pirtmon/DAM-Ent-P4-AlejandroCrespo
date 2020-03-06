06d2238ac1ef67edb56864dd5ca7f5db751ac01b
DAM_1 <DAM_1@PB01>
2020-03-06 21:07:41 +0100

    todo

--- src/main/java/liceosorolla/Numero.java
+++ src/main/java/liceosorolla/Numero.java
@@ -2,4 +2,101 @@ package main.java.liceosorolla;
 
 public class Numero {
 
+	private int numero1;
+	private int numero2;
+
+	public Numero(int numero1, int numero2) {
+
+		this.numero1 = numero1;
+		this.numero2 = numero2;
+	}
+
+	public String toString() {
+		return "numero1=" + numero1 + " numero2=" + numero2;
+	}
+
+	public int getNumero1() {
+		return numero1;
+	}
+
+	public void setNumero1(int numero1) {
+		this.numero1 = numero1;
+	}
+
+	public int getNumero2() {
+		return numero2;
+	}
+
+	public void setNumero2(int numero2) {
+		this.numero2 = numero2;
+	}
+
+	public int suma() {
+		int suma = 0;
+
+		suma = this.numero1 + this.numero2;
+
+		return suma;
+
+	}
+
+	public int resta() {
+		int resta = 0;
+
+		resta = this.numero2 - this.numero1;
+
+		return resta;
+
+	}
+
+	public int multiplicacion() {
+		int multiplicacion = 0;
+
+		multiplicacion = this.numero1 * this.numero2;
+
+		return multiplicacion;
+
+	}
+
+	public int division() {
+		int division = 0;
+
+		division = this.numero2 / this.numero1;
+
+		return division;
+
+	}
+
+	public int potencia() {
+
+		int potencia = 0;
+
+		potencia = (int) Math.pow(this.numero2, this.numero1);
+
+		return potencia;
+
+	}
+
+	public boolean multiplo() {
+		boolean multi = false;
+
+		if (this.numero1 >= this.numero2) {
+			if (this.numero1 % this.numero2 == 0) {
+
+				multi = true;
+			}
+
+		} else {
+
+			if (this.numero2 % this.numero1 == 0) {
+
+				multi = true;
+			}
+
+		}
+
+		return multi;
+
+	}
+
 }
--- src/test/java/liceosorolla/NumeroTest.java
+++ src/test/java/liceosorolla/NumeroTest.java
@@ -1,5 +1,66 @@
 package test.java.liceosorolla;
+import org.junit.Before;
+import org.junit.Test;
+import main.java.liceosorolla.Numero;
+import static org.junit.Assert.assertEquals;
 
 public class NumeroTest {
+	
+	private Numero numero;
+	
+	@Before
+	public void Before() {
+		
+		numero = new Numero(2,6);
+	}
+	
+	@Test
+	public void comprobarNum1() {
+		
+		assertEquals(2,numero.getNumero1());
+	}
+	
+	@Test
+	public void comprobarNum2() {
+		
+		assertEquals(6,numero.getNumero2());
+	}
+	
+	@Test
+	public void comprobarSuma() {
+		
+		assertEquals(8,numero.suma());
+	}
+	
+	@Test
+	public void comprobarResta() {
+		
+		assertEquals(4,numero.resta());
+	}
+	
+	@Test
+	public void comprobarMultiplicar() {
+		
+		assertEquals(12,numero.multiplicacion());
+	}
+	
+	@Test
+	public void comprobarDividir() {
+		
+		assertEquals(3,numero.division());
+	}
+	
+	@Test
+	public void comprobarPotencia() {
+		
+		assertEquals(36,numero.potencia());
+	}
+	
+	@Test
+	public void comprobarmultiplo() {
+		
+		assertEquals(true,numero.multiplo());
+	}
+	
 
 }
